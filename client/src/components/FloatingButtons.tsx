243aa09a04ca219e2cac15972d5a59a658a0cb7a
jenniferthiane <jennifer.thiane@hotmail.com>
2025-04-27 15:15:42 -0300

    Delete client directory

--- client/src/components/FloatingButtons.tsx
+++
@@ -1,7 +0,0 @@
-// FloatingButtons está vazio agora pois foi removido conforme solicitado
-
-const FloatingButtons = () => {
-  return null; // Não renderiza nada
-};
-
-export default FloatingButtons;
